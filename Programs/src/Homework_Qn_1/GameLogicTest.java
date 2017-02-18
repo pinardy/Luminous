3839d96e31119ec9ae80a5ff7cbd97a0f14cc159
Kenneth Lim <kennethlimcp@gmail.com>
2017-02-18 22:22:39 +0800

    refactor: move mocking to beforeTest
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- Programs/src/Homework_Qn_1/GameLogicTest.java
+++ Programs/src/Homework_Qn_1/GameLogicTest.java
@@ -21,6 +21,10 @@ public class GameLogicTest {
     public void runBeforeEachTest() {
         gameLogic = new GameLogic();
         Mockery context = new JUnit4Mockery();
+
+        final Pillar pillar = context.mock(pillar.class);
+        final Shadow shadow = context.mock(shadow.class);
+        final Core core = context.mock(core.class);
     }
 
     @After
@@ -29,52 +33,43 @@ public class GameLogicTest {
         context = null;
     }
 
-
-
     @Test
     public void shadowAttCoreIfTest() {
-        final Pillar pillar = context.mock(pillar.class);
-
-        context.checking(new Expectations() {{
+        context.checking(new Expectations() {
             oneOf(pillar).lightStatus;
             will(returnValue(1));
-        }});
+        });
 
         assertEquals(1, gameLogic.shadowAttCore());
     }
 
     @Test
     public void shadowAttCoreElseTest() {
-        final Pillar pillar = context.mock(pillar.class);
-        final Shadow shadow = context.mock(shadow.class);
-
-        context.checking(new Expectations() {{
+        context.checking(new Expectations() {
             oneOf(pillar).lightStatus;
             will(returnValue(0));
             oneOf(shadow).attackCore();
-        }});
+        });
 
         assertEquals(0, gameLogic.shadowAttCore());
     }
 
     @Test
     public void checkGameStatusIfTest() {
-        context.checking(new Expectations() {{
+        context.checking(new Expectations() {
             oneOf(core).getHealth;
             will(returnValue(0));
-        }});
+        });
 
         assertEquals(0, gameLogic.checkGameStatus());
     }
 
     @Test
     public void checkGameStatusElseTest() {
-        final Core core = context.mock(core.class);
-
-        context.checking(new Expectations() {{
+        context.checking(new Expectations() {
             oneOf(core).getHealth;
             will(returnValue(10));
-        }});
+        });
 
         assertEquals(1, gameLogic.checkGameStatus());
     }
