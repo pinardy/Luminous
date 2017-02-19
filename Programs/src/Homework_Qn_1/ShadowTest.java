c1a264922d25004c94351a704338407dad776cb8
polarvenezia <polarvenezia@gmail.com>
2017-02-19 22:37:31 +0800

    Create ShadowTest.java

---
+++ Programs/src/Homework_Qn_1/ShadowTest.java
@@ -0,0 +1,39 @@
+package Week4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by polarvenezia on 19/2/17.
+ */
+public class ShadowTest {
+    Core core;
+    UserInterface ui;
+    @Before
+    public void runBeforeTest(){
+        core = new Core();
+        ui = new UserInterface();
+    }
+
+    @Test
+    public void testWithCore(){
+        int currentHealth = core.getHealth();
+        Shadow.attackCore(core, ui);
+        assertEquals(currentHealth-5, core.getHealth());
+    }
+
+    @Test
+    public void testWithUI(){
+        int currentHealth = core.getHealth();
+        Shadow.attackCore(core, ui);
+        assertEquals(currentHealth-5, ui.getHealth());
+    }
+
+
+    @After
+    public void runAfterTest(){
+        core = null;
+        ui = null;
+    }
+}
