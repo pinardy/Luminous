b143ddb27411cff602b900836829785c6e19ff02
Kenneth Lim <kennethlimcp@gmail.com>
2017-03-08 15:58:50 +0800

    add: new shadow every 10 seconds
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- core/src/com/mygdx/game/ShadowManagement.java
+++ core/src/com/mygdx/game/ShadowManagement.java
@@ -5,8 +5,11 @@ import com.mygdx.game.Screens.PlayScreen;
 import com.mygdx.game.Sprites.Shadow;
 
 import java.util.ArrayList;
+<<<<<<< HEAD
 import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
+=======
+>>>>>>> c70d6ed... add: new shadow every 10 seconds
 
 /**
  * Created by kennethlimcp on 08/Mar/2017.
@@ -14,8 +17,12 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ShadowManagement extends Thread {
     private MultiplayerGame game = null;
+<<<<<<< HEAD
     private CopyOnWriteArrayList<Shadow> shadows = new CopyOnWriteArrayList<Shadow>();
     private final Object shadowsLock = new Object();
+=======
+    private ArrayList<Shadow> shadows = new ArrayList<Shadow>();
+>>>>>>> c70d6ed... add: new shadow every 10 seconds
 
     public ShadowManagement(MultiplayerGame game) {
         this.game = game;
@@ -24,6 +31,7 @@ public class ShadowManagement extends Thread {
     @Override
     public void run() {
         calculateShadowStartPosition();
+<<<<<<< HEAD
 
         Random rand = new Random();
         long oldTime = System.currentTimeMillis();
@@ -44,12 +52,29 @@ public class ShadowManagement extends Thread {
                     }
                 }
             }
+=======
+        //this will draw all the shawdow for testing as we prefer to create them randomly
+        long oldTime = System.currentTimeMillis();
+
+        for(Rectangle r: game.getPillarPositions()) {
+            while(System.currentTimeMillis() - oldTime < 10000) {};
+            shadows.add(new Shadow((PlayScreen) game.getScreen(), r.getX(), r.getY()));
+            shadows.get(shadows.size()-1).update(1);
+            oldTime = System.currentTimeMillis();
+            System.out.println("Shadow created!");
+        }
+
+        while(true) {
+>>>>>>> c70d6ed... add: new shadow every 10 seconds
 
         }
     }
 
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> c70d6ed... add: new shadow every 10 seconds
     private void calculateShadowStartPosition() {
         float coreX = game.corePosition.getX() + game.corePosition.getWidth()/2;
         float coreY = game.corePosition.getY() + game.corePosition.getHeight()/2;
@@ -68,10 +93,15 @@ public class ShadowManagement extends Thread {
     }
 
     public void update(float dt) {
+<<<<<<< HEAD
         synchronized (shadowsLock) {
             for(Shadow s: shadows) {
                 s.update(dt);
             }
+=======
+        for(Shadow s: shadows) {
+            s.update(dt);
+>>>>>>> c70d6ed... add: new shadow every 10 seconds
         }
     }
 }
