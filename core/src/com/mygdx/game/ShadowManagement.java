70d3b8ad14e4f1712409ea01a5ea20f0c53d7920
Kenneth Lim <kennethlimcp@gmail.com>
2017-03-15 15:19:49 +0800

    fix: rebase weirdness
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- core/src/com/mygdx/game/ShadowManagement.java
+++ core/src/com/mygdx/game/ShadowManagement.java
@@ -1,16 +1,10 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.mygdx.game.Screens.PlayScreen;
 import com.mygdx.game.Sprites.Shadow;
-
-import java.util.ArrayList;
-<<<<<<< HEAD
 import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
-=======
->>>>>>> c70d6ed... add: new shadow every 10 seconds
 
 /**
  * Created by kennethlimcp on 08/Mar/2017.
@@ -18,12 +12,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ShadowManagement extends Thread {
     private MultiplayerGame game = null;
-<<<<<<< HEAD
     private CopyOnWriteArrayList<Shadow> shadows = new CopyOnWriteArrayList<Shadow>();
     private final Object shadowsLock = new Object();
-=======
-    private ArrayList<Shadow> shadows = new ArrayList<Shadow>();
->>>>>>> c70d6ed... add: new shadow every 10 seconds
 
     public ShadowManagement(MultiplayerGame game) {
         this.game = game;
@@ -32,13 +22,10 @@ public class ShadowManagement extends Thread {
     @Override
     public void run() {
         calculateShadowStartPosition();
-<<<<<<< HEAD
-
         Random rand = new Random();
-        long oldTime = System.currentTimeMillis();
 
-        while(true) {
-            if(shadows.size() == 0) {
+        while (true) {
+            if (shadows.size() == 0) {
                 int randomShadow = rand.ints(1, 0, game.getPillarPositions().size()).findFirst().getAsInt();
 
                 Rectangle r = game.getPillarPositions().get(randomShadow);
@@ -47,35 +34,15 @@ public class ShadowManagement extends Thread {
             }
 
             synchronized (shadowsLock) {
-                for(Shadow s: shadows) {
+                for (Shadow s : shadows) {
                     if (!s.isAlive()) {
                         shadows.remove(s);
                     }
                 }
             }
-=======
-        //this will draw all the shawdow for testing as we prefer to create them randomly
-        long oldTime = System.currentTimeMillis();
-
-        for(Rectangle r: game.getPillarPositions()) {
-            while(System.currentTimeMillis() - oldTime < 10000) {};
-
-            shadows.add(new Shadow((PlayScreen) game.getScreen(), r.getX(), r.getY()));
-            oldTime = System.currentTimeMillis();
-            System.out.println("Shadow created!");
-        }
-
-        while(true) {
->>>>>>> c70d6ed... add: new shadow every 10 seconds
-
         }
     }
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> c70d6ed... add: new shadow every 10 seconds
     private void calculateShadowStartPosition() {
         float coreX = game.corePosition.getX() + game.corePosition.getWidth()/2;
         float coreY = game.corePosition.getY() + game.corePosition.getHeight()/2;
@@ -94,15 +61,10 @@ public class ShadowManagement extends Thread {
     }
 
     public void update(float dt) {
-<<<<<<< HEAD
         synchronized (shadowsLock) {
-            for(Shadow s: shadows) {
+            for (Shadow s : shadows) {
                 s.update(dt);
             }
-=======
-        for(Shadow s: shadows) {
-            s.update(dt);
->>>>>>> c70d6ed... add: new shadow every 10 seconds
         }
     }
 }
