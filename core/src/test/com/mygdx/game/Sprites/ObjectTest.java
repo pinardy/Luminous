6f0736bb76775200ba437029d428159dc65d84a1
Kenneth Lim <kennethlimcp@gmail.com>
2017-04-02 22:11:24 +0800

    add: Object unit test
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

---
+++ core/src/test/com/mygdx/game/Sprites/ObjectTest.java
@@ -0,0 +1,57 @@
+package com.mygdx.game.Sprites;
+
+import com.mygdx.game.Screens.PlayScreen;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Created by kennethlimcp on 30/Mar/2017.
+ */
+public class ObjectTest extends TestCase {
+    PlayScreen playScreen;
+    float testX;
+    float testY;
+    ObjectMethod o;
+
+
+    public void setUp() {
+        PlayScreen playScreen = Mockito.mock(PlayScreen.class);
+        float testX = 1;
+        float testY = 1;
+
+        o = new ObjectMethod(playScreen, testX, testY);
+    }
+
+    @Test
+    public void testAbstractDefineObject() throws Exception {
+
+        assertEquals(o.getBooleanFlag(), false);
+        o.defineObject();
+        assertEquals(o.getBooleanFlag(), true);
+    }
+
+
+}
+
+class ObjectMethod extends Object {
+    private boolean defineObjectIsCalledInConstructor;
+
+    public ObjectMethod(PlayScreen screen, float x, float y) {
+        super(screen, x, y);
+        defineObjectIsCalledInConstructor = false;
+
+    }
+
+    @Override
+    protected void defineObject() {
+        defineObjectIsCalledInConstructor = true;
+    }
+
+    boolean getBooleanFlag() {
+        return defineObjectIsCalledInConstructor;
+    }
+}
