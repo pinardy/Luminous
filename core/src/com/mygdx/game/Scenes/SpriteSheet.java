002ae4113d4060ce8b96971830d4a3ef29c37214
shajinihubert <shajini_hubert@mymail.sutd.edu.sg>
2017-04-04 00:04:01 +0800

    edit for sprite sheet

---
+++ core/src/com/mygdx/game/Scenes/SpriteSheet.java
@@ -0,0 +1,43 @@
+package com.mygdx.game.Scenes;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+/**
+ * Created by User on 30-Mar-17.
+ */
+
+public class SpriteSheet {
+    private String path;
+    private int width;
+    private int height;
+    private int[] pixels;
+
+    public SpriteSheet(String path){
+        BufferedImage image = null;
+
+        try {
+            image = ImageIO.read(SpriteSheet.class.getResourceAsStream(path));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        if (image == null){
+            return;
+        }
+
+        this.path = path;
+        this.height = image.getHeight();
+        this.width = image.getWidth();
+        pixels = image.getRGB(0,0,width,height,null,0,width);
+
+        for (int i = 0; i < pixels.length; i++){
+            //0xAARRGGBB
+            //255/4 = 63.75 =~ 64 : divide by 4 because 4 monochromatic colours
+            pixels[i] = pixels[i]/(64);
+        }
+    }
+
+}
