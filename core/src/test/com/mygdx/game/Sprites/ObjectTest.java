13e92eb45c16112889c5721f3be24f016444464f
Kenneth Lim <kennethlimcp@gmail.com>
2017-04-04 11:10:31 +0800

    add: tearDown for ObjectTest
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- core/src/test/com/mygdx/game/Sprites/ObjectTest.java
+++ core/src/test/com/mygdx/game/Sprites/ObjectTest.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.Sprites;
 
+import com.badlogic.gdx.physics.box2d.Filter;
 import com.mygdx.game.Screens.PlayScreen;
 
 import junit.framework.TestCase;
@@ -7,6 +8,8 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
 
 /**
  * Created by kennethlimcp on 30/Mar/2017.
@@ -28,6 +31,15 @@ public class ObjectTest extends TestCase {
         o = new ObjectMethod(playScreen, testX, testY);
     }
 
+
+    public void tearDown() throws Exception {
+        PlayScreen playScreen = null;
+        float testX = 0;
+        float testY = 0;
+
+        o = null;
+    }
+
     @Test
     public void testAbstractDefineObject() throws Exception {
 
@@ -35,7 +47,6 @@ public class ObjectTest extends TestCase {
         o.defineObject();
         assertEquals(o.getBooleanFlag(), true);
     }
-
 
 }
 
